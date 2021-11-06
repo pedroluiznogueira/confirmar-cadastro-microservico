5c589c479fe9df945c3a0279daec5d81dfc09ece
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-11-05 23:55:34 -0300

    Me comunicando para a API principal da Udeyou por este Microserviço por meio do Feign do Spring Cloud

--- src/main/java/com/confirmarcadastro/microservico/MicroservicoApplication.java
+++ src/main/java/com/confirmarcadastro/microservico/MicroservicoApplication.java
@@ -2,8 +2,10 @@ package com.confirmarcadastro.microservico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MicroservicoApplication {
 
 	public static void main(String[] args) {
