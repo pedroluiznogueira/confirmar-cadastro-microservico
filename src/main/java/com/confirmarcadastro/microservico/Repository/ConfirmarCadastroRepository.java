e4d067c4b7f30ab0cf89843915120587e4a7b4a2
Raphael Seabra <77645109+seabraph@users.noreply.github.com>
2021-11-07 19:11:59 -0300

    alterando endpoint confirmarCadastroRepository

--- src/main/java/com/confirmarcadastro/microservico/Repository/ConfirmarCadastroRepository.java
+++ src/main/java/com/confirmarcadastro/microservico/Repository/ConfirmarCadastroRepository.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(url= "http://localhost:8080/" , name = "confirmar")
+@FeignClient(url= "https://udeyou-api.herokuapp.com/" , name = "confirmar")
 public interface ConfirmarCadastroRepository {
 
     @PostMapping("cadastro")
